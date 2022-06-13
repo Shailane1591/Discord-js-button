925deb9ae1af0ff913a4a250a31319b69eb9a73b
Shailane <107360148+Shailane1591@users.noreply.github.com>
2022-06-13 13:05:54 +0700

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,12 @@
+/* 
+    
+    Return Button For Bot Discord JS
+
+    > Usage nodejs
+    > modulejs for package
+
+*/
+
+return interaction.reply({
+  embeds: [new MessageEmbed().setDescription("Halo Selamat Datang").setColor(RANDOM)]
+}) 
